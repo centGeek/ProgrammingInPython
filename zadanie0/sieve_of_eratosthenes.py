45406612694aa39d7f70f332d09a235a45ca4ee7
247644 Dominiak Maciej <247644@edu.p.lodz.pl>
2024-10-14 13:09:28 +0200

    Code works, maybe we can add some optimisation.

--- zadanie0/sieve_of_eratosthenes.py
+++ zadanie0/sieve_of_eratosthenes.py
@@ -1,4 +1,21 @@
+import math
+from math import sqrt
+from math import ceil
+
 amount_of_numbers = 100
-print(f"Wielkosc sita wynosi: {amount_of_numbers}")
-def sieve_of_eratosthenes():
-    
+
+
+def sieve_of_eratosthenes(primes_to_this_number):
+    tab = [True] * primes_to_this_number
+    primes = []
+    for i in range(2, primes_to_this_number):
+        if tab[i]:
+            primes.append(i)
+            for j in range(i * i, primes_to_this_number, i):
+                tab[j] = False
+    return primes
+
+
+# print(f"Wielkosc sita wynosi: {amount_of_numbers}")
+
+print(f"Prime numbers to {amount_of_numbers} {sieve_of_eratosthenes(amount_of_numbers)}")
